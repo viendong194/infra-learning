8b91067173c33616f7607142af96f63425645246
DongVHV <dongvhv@kaopiz.com>
2025-01-03 17:23:55 +0700

    fix docker-compose, add retryconnect2mysql to node/index.js

--- docker/backend/index.js
+++ docker/backend/index.js
@@ -1,21 +1,49 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const mysql = require("mysql2");
 const app = express();
 const port = 8000;
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+};
+let db;
+function connectWithRetry() {
+  db = mysql.createConnection(dbConfig);
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  db.connect((err) => {
+    if (err) {
+      console.error("Error connecting to MySQL:", err);
+      setTimeout(connectWithRetry, 30000); // Retry after 30s seconds
+      return;
+    }
+    console.log("Connected to MySQL");
+    runMigrations(db);
   });
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL:", err);
-    return;
-  }
-  console.log("Connected to MySQL");
-});
+
+  return db;
+}
+
+function runMigrations() {
+  const migrationFilePath = path.join(__dirname, "migration.sql");
+  const migrationSQL = fs.readFileSync(migrationFilePath, "utf-8");
+  const migrationCommands = migrationSQL.split(';').filter(cmd => cmd.trim() !== '');
+
+  migrationCommands.forEach((command) => {
+    db.query(command, (err, results) => {
+      if (err) {
+        console.error("Error running migration command:", err);
+        return;
+      }
+      console.log("Migration command ran successfully");
+    });
+  });
+}
+
+connectWithRetry();
 
 app.get("/users", (req, res) => {
   db.query("SELECT id, name, email FROM users", (err, results) => {
@@ -29,5 +57,5 @@ app.get("/users", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Backend API listening at http://localhost:${port}`);
+  console.log(`Backend API listening at http://node:${port}`);
 });
--- docker/backend/migration.sql
+++ docker/backend/migration.sql
@@ -1,12 +1,9 @@
 CREATE DATABASE IF NOT EXISTS appdb;
-
 USE appdb;
-
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL
 );
-
 INSERT INTO users (name, email) VALUES ('dongvhv', 'dongvhv@example.com');
 INSERT INTO users (name, email) VALUES ('nhanhb', 'nhanhb@example.com');
